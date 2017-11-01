60412457d35ce381ddcfedbf3b8c2f84f2fbb33b
David E Jones <dej@dejc.com>
2017-10-31 23:09:50 -0700

    Fix copyright headers, gateway config ID

--- service/mantle/account/script/Authorize.groovy
+++ service/mantle/account/script/Authorize.groovy
@@ -1,5 +1,15 @@
 /*
- * Copyright 2017 RidgeCrest Herbals. All Rights Reserved.
+ * This software is in the public domain under CC0 1.0 Universal plus a
+ * Grant of Patent License.
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
+ * public domain worldwide. This software is distributed without any
+ * warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software (see the LICENSE.md file). If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 import com.braintreegateway.BraintreeGateway
 import com.braintreegateway.CustomerRequest
@@ -98,7 +108,7 @@ if (result.isSuccess()) {
 
     String avsCode = transaction.avsErrorResponseCode?:(transaction.avsPostalCodeResponseCode + transaction.avsStreetAddressResponseCode)
     ec.service.sync().name("create#mantle.account.method.PaymentGatewayResponse").parameters([
-            paymentGatewayConfigId:"RchBraintree", paymentOperationEnumId:"PgoAuthorize", paymentId:paymentId,
+            paymentGatewayConfigId:paymentGatewayConfigId, paymentOperationEnumId:"PgoAuthorize", paymentId:paymentId,
             paymentMethodId:paymentMethodId, amountUomId:payment.amountUomId, amount:transaction.amount,
             referenceNum:transaction.id, responseCode:transaction.processorResponseCode, reasonMessage: result.message,
             transactionDate:ec.user.nowTimestamp, avsResult:avsCode, cvResult:transaction.cvvResponseCode,
@@ -160,7 +170,7 @@ if (result.isSuccess()) {
 
         String avsCode = transaction.avsErrorResponseCode ?: (transaction.avsPostalCodeResponseCode?:"" + transaction.avsStreetAddressResponseCode?:"")
         ec.service.sync().name("create#mantle.account.method.PaymentGatewayResponse").parameters([
-                paymentGatewayConfigId: "RchBraintree", paymentOperationEnumId: "PgoAuthorize", paymentId: paymentId,
+                paymentGatewayConfigId:paymentGatewayConfigId, paymentOperationEnumId: "PgoAuthorize", paymentId: paymentId,
                 paymentMethodId:paymentMethodId, amountUomId: payment.amountUomId, amount: transaction.amount,
                 referenceNum:transaction.id, responseCode: transaction.processorResponseCode, reasonMessage: responseText,
                 transactionDate:ec.user.nowTimestamp, avsResult: avsCode, cvResult: transaction.cvvResponseCode,
